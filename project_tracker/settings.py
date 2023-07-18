e726d018d06084daf07b93998df9929ef6fbd1f1
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-07-18 15:39:53 +0530

    Setting file of main project

--- project_tracker/settings.py
+++ project_tracker/settings.py
@@ -149,9 +149,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Authentication
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_FILTER_BACKENDS': [
-    #     'django_filters.rest_framework.DjangoFilterBackend'
-    # ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
