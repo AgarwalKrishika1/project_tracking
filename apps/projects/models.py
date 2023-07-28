3c21395c705ad39921b5a9184f88cb1e80cfd400
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-07-28 18:09:36 +0530

    Projects app with project_manager validator

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,4 +1,6 @@
 from enum import Enum
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from apps.users.models import User, UserProfile
 from apps.master.models import ProjectCategory
@@ -24,12 +26,18 @@ class Base(models.Model):
 
 
 class Projects(Base):
+    def validate_project_manager_role(value):
+        if value and not UserProfile.objects.filter(id=value.id, role='project_manager').exists():
+            raise ValidationError("The selected project manager does not have the 'manager' role.")
+
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, null=True)
-    status = models.CharField(max_length=255, choices=ProjectStatus.project_status_choice(), default=ProjectStatus.INACTIVE.value)
+    status = models.CharField(max_length=255, choices=ProjectStatus.project_status_choice(),
+                              default=ProjectStatus.INACTIVE.value)
     logo = models.ImageField(upload_to='project_logo', null=True, blank=True)
-    project_manager = models.ForeignKey(UserProfile, related_name='project_manager', on_delete=models.SET_NULL, null=True)
+    project_manager = models.ForeignKey(UserProfile, related_name='project_manager', on_delete=models.SET_NULL,
+                                        null=True, validators=[validate_project_manager_role])
 
     def __str__(self):
         return self.name
--- apps/projects/tests/test_project.py
+++ apps/projects/tests/test_project.py
@@ -1,16 +1,12 @@
 import json
-
 from apps.base.test_base import BaseTestCase
-from apps.users.models import UserProfile
-from apps.projects.models import Projects
 from rest_framework import status
-from apps.base.functions import create_userprofile
 
 
 class ProjectTestCase(BaseTestCase):
     def create_project(self):
         data = {
-            "name": "test",
+            "name": "tests",
             "description": "testing",
         }
         response = self.authorized_pm.post("/clients/projects/", data=data, format='json')
@@ -19,10 +15,72 @@ class ProjectTestCase(BaseTestCase):
     def test_get_projects(self):
         res = self.create_project()
         response = self.authorized_pm.get("/clients/projects/")
-        self.assertContains(response, "test")
-        self.assertContains(response, "testing")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         # converts byte data string using json.loads()
         data = json.loads(response.content)
-
         print("Response Data:", data)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_with_unauthorised(self):
+        res = self.create_project()
+        response = self.unauthorized_srd.get("/clients/projects/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_with_pm(self):
+        res = self.create_project()
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        response = self.authorized_pm.get("/clients/projects/")
+        self.assertContains(response, '"name":"tests"')
+
+    def test_create_with_srd(self):
+        data = {
+            "name": "tests",
+            "description": "testing",
+        }
+        response = self.authorized_srd.post("/clients/projects/", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_with_pm(self):
+        res = self.create_project()
+        data = {
+            "name": "test123",
+            "description": "testing",
+        }
+        response = self.authorized_pm.patch("/clients/projects/{}/".format(res.data['id']), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.authorized_pm.get("/clients/projects/")
+        self.assertContains(response, '"name":"test123"')
+
+    def test_update_with_jr_developer(self):
+        res = self.create_project()
+        data = {
+            "name": "test123",
+            "description": "testing",
+        }
+        response = self.authorized_jrd.patch("/clients/projects/{}/".format(res.data['id']), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_with_unauthorised(self):
+        res = self.create_project()
+        data = {
+            "name": "test123",
+            "description": "testing",
+        }
+        response = self.unauthorized_pm.patch("/clients/projects/{}/".format(res.data['id']), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_with_pm(self):
+        res = self.create_project()
+        response = self.authorized_pm.delete("/clients/projects/{}/".format(res.data['id']))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_with_developer(self):
+        res = self.create_project()
+        response = self.authorized_srd.delete("/clients/projects/{}/".format(res.data['id']))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_with_unauthorised(self):
+        res = self.create_project()
+        response = self.unauthorized_admin.delete("/clients/projects/{}/".format(res.data['id']))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
