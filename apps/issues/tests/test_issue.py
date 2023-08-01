49e5b90918c5aeda81da367c63fc25554bdefba0
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-08-01 16:24:52 +0530

    Test file of issue test case

--- apps/issues/tests/test_issue.py
+++ apps/issues/tests/test_issue.py
@@ -3,7 +3,7 @@ from apps.base.test_base import BaseTestCase
 from rest_framework import status
 
 
-class ProjectTestCase(BaseTestCase):
+class IssueTestCase(BaseTestCase):
     def create_issue(self):
         data = {
             "title": "issue trial",
@@ -54,7 +54,7 @@ class ProjectTestCase(BaseTestCase):
             "status": "in_progress",
             "priority": "medium"
         }
-        response = self.authorized_pm.patch("/issues/{}/".format(res.data['id']), data=data, format='json')
+        response = self.authorized_pm.patch(f"/issues/{res.data.get('id')}/", data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response = self.authorized_pm.get("/issues/")
         self.assertContains(response, '"title":"issue123"')
@@ -68,15 +68,45 @@ class ProjectTestCase(BaseTestCase):
             "status": "in_progress",
             "priority": "medium"
         }
-        response = self.unauthorized_pm.patch("/issues/{}/".format(res.data['id']), data=data, format='json')
+        response = self.unauthorized_pm.patch(f"/issues/{res.data.get('id')}/", data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_delete_with_authorised(self):
         res = self.create_issue()
-        response = self.authorized_srd.delete("/issues/{}/".format(res.data['id']))
+        response = self.authorized_srd.delete(f"/issues/{res.data.get('id')}/")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_with_unauthorised(self):
         res = self.create_issue()
-        response = self.unauthorized_admin.delete("/issues/{}/".format(res.data['id']))
+        response = self.unauthorized_admin.delete(f"/issues/{res.data.get('id')}/")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class IssueFilterTestCase(IssueTestCase):
+    def test_filter_on_type(self):
+        res = self.create_issue()
+        url = "/issues/" + f"?type=task"
+        response = self.authorized_srd.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filter_on_status(self):
+        res = self.create_issue()
+        url = "/issues/" + f"?status=in_progress"
+        response = self.authorized_srd.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filter_on_priority(self):
+        res = self.create_issue()
+        url = "/issues/" + f"?priority=medium"
+        response = self.authorized_srd.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_search_filter_on_title(self):
+        res = self.create_issue()
+        url = "/issues/" + f"?title=issue trial"
+        response = self.authorized_srd.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
