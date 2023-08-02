835aa4cd29cea7cd175146f40532ee1fdf60a632
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-08-02 17:21:12 +0530

    Master app with removal of extra code

--- apps/master/models.py
+++ apps/master/models.py
@@ -1,18 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class ProjectCategory(models.Model):
-
-    # class Pcategory(models.TextChoices):
-        # production = 'production', 'Production',
-        # web_development = 'web_development', 'Web Development',
-        # social = 'social', 'Social',
-        # educational = 'educational', 'Educational',
-        # research = 'research', 'Research'
-
     name = models.CharField()
 
     def __str__(self):
