f80a073564266b8c78f4f9be0c622c6445e7898d
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-08-09 17:47:49 +0530

    Test comment with filters test

--- apps/comments/tests/test_comment.py
+++ apps/comments/tests/test_comment.py
@@ -7,18 +7,18 @@ from apps.base.test_base import BaseTestCase
 from rest_framework import status
 from apps.issues.models import Issues
 
-PM_USER = "pm "
-PM_PASSWORD = "1234"
-PM_EMAIL = "testpm@gmail.com"
+USER = "user "
+PASSWORD = "1234"
+EMAIL = "testuser@gmail.com"
 
 
 class CommentTest(BaseTestCase):
     @classmethod
     def setUpTestData(cls):
         super(BaseTestCase, cls).setUpTestData()
-        project_manager = create_userprofile(username=PM_USER, email=PM_EMAIL, password=PM_PASSWORD,
-                                             role="project_manager")
-        cls.project_manager_userprofile = project_manager
+        user = create_userprofile(username=USER, email=EMAIL, password=PASSWORD,
+                                  role="project_manager")
+        cls.user_userprofile = user
 
     def setUp(self) -> None:
         issue_data = {
@@ -31,7 +31,7 @@ class CommentTest(BaseTestCase):
         self.issue = Issues.objects.create(**issue_data)
 
         super(BaseTestCase, self).setUp()
-        self.project_manager = User.objects.filter(username=PM_USER).first()
+        self.project_manager = User.objects.filter(username=USER).first()
         pm_token = RefreshToken.for_user(self.project_manager)
         pm_access_key = str(pm_token.access_token)
         self.unauthorized_pm = APIClient(self.project_manager)
@@ -41,7 +41,7 @@ class CommentTest(BaseTestCase):
     def create_comment(self):
         data = {
             "text": "comment1",
-            "created_by": self.project_manager_userprofile.id,
+            "created_by": self.user_userprofile.id,
             "issue": self.issue.id
         }
         response = self.authorized_pm.post("/comments/", data=data, format='json')
@@ -51,7 +51,7 @@ class CommentTest(BaseTestCase):
     def create_comment_with_unauthorised(self):
         data = {
             "text": "comment1",
-            "created_by": self.project_manager_userprofile.id,
+            "created_by": self.user_userprofile.id,
             "issue": self.issue.id
         }
         response = self.unauthorized_pm.post("/comments/", data=data, format='json')
@@ -61,7 +61,7 @@ class CommentTest(BaseTestCase):
     def update_comment_with_authorised(self):
         data = {
             "text": "comment1",
-            "created_by": self.project_manager_userprofile.id,
+            "created_by": self.user_userprofile.id,
             "issue": self.issue.id
         }
         response = self.authorized_pm.post("/comments/", data=data, format='json')
@@ -71,7 +71,7 @@ class CommentTest(BaseTestCase):
     def update_comment_with_unauthorised(self):
         data = {
             "text": "comment1",
-            "created_by": self.project_manager_userprofile.id,
+            "created_by": self.user_userprofile.id,
             "issue": self.issue.id
         }
         response = self.unauthorized_pm.post("/comments/", data=data, format='json')
@@ -119,4 +119,34 @@ class CommentTest(BaseTestCase):
         res = self.create_comment()
         url = f"/comments/{res.data.get('id')}/"
         response = self.unauthorized_pm.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestCommentFilter(CommentTest):
+    def test_filter_on_text(self):
+        res = self.create_comment()
+        url = "/comments/" + f"?text=comment1"
+        response = self.authorized_pm.get(url)
+        data = json.loads(response.content)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filter_text_unauthorised(self):
+        res = self.create_comment()
+        url = "/comments/" + f"?text=comment1"
+        response = self.unauthorized_pm.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_filter_created_by(self):
+        res = self.create_comment()
+        url = "/comments/" + f"?created_by={self.user_userprofile.id}"
+        response = self.authorized_pm.get(url)
+        data = json.loads(response.content)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filter_created_by_unauthorised(self):
+        res = self.create_comment()
+        url = "/comments/" + f"?created_by={self.user_userprofile.id}"
+        response = self.unauthorized_pm.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
