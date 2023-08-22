e31893eafa801a7610efe174ce422fca465c7a30
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-08-22 12:41:07 +0530

    Added filterset class for comment model

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,20 +1,20 @@
+from apps.comments.filters import CommentFilterSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
 from apps.comments.serializer import CommentSerializer, CommentReadOnlySerializer
 from rest_framework.viewsets import ModelViewSet
 from apps.comments.models import Comment
 
 
 class CommentViewSet(ModelViewSet):
-    queryset = Comment.objects.all()
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    queryset = Comment.objects.filter(is_delete=False)
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ["created_by"]
     search_fields = ["text"]
+    filterset_class = CommentFilterSet
 
     def get_serializer_class(self):
         if self.request.method == "GET":
             return CommentReadOnlySerializer
         return CommentSerializer
 
-    def get_queryset(self):
-        return Comment.objects.filter(is_delete=False)
+
