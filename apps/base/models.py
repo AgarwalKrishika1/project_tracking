4cf62e6a67bb9209fcd75279bb9d49c55f25d58e
AgarwalKrishika <135822185+AgarwalKrishika@users.noreply.github.com>
2023-08-22 12:36:41 +0530

    Changed DateTime field to Date field

--- apps/base/models.py
+++ apps/base/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Base(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         abstract = True
